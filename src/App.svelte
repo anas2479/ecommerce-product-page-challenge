2cb9e7d69ace5c4597e90dd127dcdc5b4fea6ccc
Anas Mohamed <amohamed2479@gmail.com>
2021-10-21 08:58:25 -0500

    Create App.svelte
    put everything together

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<div class="">
+<Menu/>
+<ProductView/>
+</div>
+
+
+<script>
+
+import Menu  from "./components/menu.svelte";
+import ProductView from "./components/product-view.svelte";
+
+</script>
